68f27740f580de5a4d97bb37d439d7db84e794f5
DarioKnezovic <dknezovic997@gmail.com>
2023-11-14 21:34:43 +0000

    Added Customer key in User.

--- internal/user/user.go
+++ internal/user/user.go
@@ -7,14 +7,15 @@ import (
 
 // User represents a user entity.
 type User struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
-	FirstName string         `gorm:"not null" json:"first_name"`
-	LastName  string         `gorm:"not null" json:"last_name"`
-	Email     string         `gorm:"unique;not null" json:"email"`
-	Password  string         `gorm:"not null" json:"password"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	FirstName   string         `gorm:"not null" json:"first_name"`
+	LastName    string         `gorm:"not null" json:"last_name"`
+	Email       string         `gorm:"unique;not null" json:"email"`
+	CustomerKey string         `json:"customer_key"`
+	Password    string         `gorm:"not null" json:"password"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	UpdatedAt   time.Time      `json:"updatedAt"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 type LoginResponse struct {
